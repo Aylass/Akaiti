948ad5cf73fc2ffa3f2c6b97119141de44347bdc
Aylass <viingoflaas@gmail.com>
2020-09-18 18:03:08 -0300

    Main Menu com botão passando para o level1

---
+++ Assets/Scripts/btplayScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btplayScript : MonoBehaviour
+{
+    public void Jogar(){
+       Application.LoadLevel("Level1");
+    }
+}
