01cecbd2bfabba35e3e8e0c0c3e5b8984ca5893c
Aylass <viingoflaas@gmail.com>
2020-10-16 18:52:29 -0300

    2 nivel completo, animações corrigidas e mecanicas prontas

---
+++ Assets/Scripts/mecanica2Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mecanica2Script : MonoBehaviour
+{
+    public GameObject canvasFim;//para desaparecer o canvas de fim de jogo e so aparecer quando for a hora
+    public GameObject canvasGanhou;//para desaparecer o canvas de ganhou e so aparecer quando for a hora
+
+    public GameObject blocoGelo;
+
+    public Transform player1;
+    public Transform player2;
+
+    void Start(){
+        canvasFim.SetActive(false);
+        canvasGanhou.SetActive(false);
+        blocoGelo.SetActive(false);
+    }
+
+    void Update(){
+        Vector3 pos1 = player1.transform.position;
+        Vector3 pos2 = player2.transform.position;
+        if((pos1.x >= 2)&&(pos1.x <= 4)){//se o jogador1 estiver perto do botao da caixa
+            if(Input.GetKey(KeyCode.E)){//retorna TRUE se o jogador apertar E para ativar o botão
+                //Debug.Log("clico");
+                blocoGelo.SetActive(true);
+            }
+        }
+
+        //se ambos os personagens chegarem a bandeira
+        if((pos1.x <= -8)&&(pos2.x >= 3)){
+            canvasGanhou.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        //Debug.Log("colisao");//aconteceu a colisao com os espinhos
+        canvasFim.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/playerPulo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPulo : MonoBehaviour
+{
+
+    private bool pulo;//true se ele ta no chao e pode pula
+  
+    public GameObject play1;
+     
+    public GameObject play2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pulo = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pulo dos personagens
+        if(pulo && Input.GetKey(KeyCode.UpArrow)){
+            //Debug.Log("Pulo");
+            play1.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,100));
+            play2.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,100));
+            pulo = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+            pulo = true;
+    }
+}
